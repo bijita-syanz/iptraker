3a306e99bf4a4b34cc63c24cda6b4e36a83ad07a
bijita-syanz <164947014+bijita-syanz@users.noreply.github.com>
2024-10-11 21:09:58 +0200

    Update main.py 2

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import json
-
+import os 
 def display_json_as_rows(file_path):
     try:
         with open(file_path, 'r') as json_file:
             data = json.load(json_file)
+            rownumber = 0
             for entry in data:
-                print("\nRow:")
+                rownumber +=1
+                print(f"\nRow  {rownumber}:")
                 for key, value in entry.items():
                     print(f"{key}: {value}")
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
     except json.JSONDecodeError:
         print("Error: Failed to decode JSON. Please check the file format.")
-
+def main() :
+  while True :
+    choise = input(">> ")
+    if choise == "show" :
+       display_json_as_rows(file_path)
+    elif choise == "rm" :
+       r = input("do you want to delete the json file (y/n) : ")
+       if r == "y" :
+           with open(file_path , "w") as file :
+               file.write(" ")
+       else :
+          pass
+    else:
+       pass
 if __name__ == "__main__":
     file_path = 'data.json'
-    display_json_as_rows(file_path)
-    print("created by bijita: v 1.0")
+    while True :
+        main()
+    print("\ncreated by bijita: v 1.0")
+
 
